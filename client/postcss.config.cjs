b8f1670a229a09eb3ed15b84d3ebedab4bc8c28b
Reen <maureenmukami531@gmail.com>
2025-11-21 05:04:50 +0300

    Delete client directory

--- client/postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-// client/postcss.config.cjs
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
--- client/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-green-600 text-white text-center py-4 mt-auto">
-      <p>© 2025 EcoTrack | Built for SDG 13: Climate Action</p>
-    </footer>
-  );
-}
